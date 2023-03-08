2981fc68b08365bc395c2433bdba56797f00d303
KaijianHuang <112913171+KaijianHuang@users.noreply.github.com>
2023-03-07 22:22:11 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,20 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
-//import the random fruit function from the lib.rs file
-use webdocker::random_movie;
+use csv::ReaderBuilder;
+use std::error::Error;
+use std::fs::File;
 
-//create a function that returns a hello world
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, random good city around the world!")
-}
-
-//create a function that returns a random best movie
-#[get("/city")]
-async fn movie() -> impl Responder {
-    //print the random movie
-    println!("Random Movie: {}", random_movie());
-    HttpResponse::Ok().body(random_movie())
-}
-
-//create a function that returns the version of the service
-#[get("/version")]
-async fn version() -> impl Responder {
-    //print the version of the service
-    println!("Version: {}", env!("CARGO_PKG_VERSION"));
-    HttpResponse::Ok().body(env!("CARGO_PKG_VERSION"))
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    //add a print message to the console that the service is running
-    println!("Running the service");
-    HttpServer::new(|| App::new().service(hello).service(movie).service(version))
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = File::open("data.csv")?;
+    let mut rdr = ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(file);
+    let mut sum = 0;
+    for result in rdr.records() {
+        let record = result?;
+        for value in record.iter() {
+            let number = value.parse::<i32>()?;
+            sum += number;
+        }
+    }
+    println!("The sum is {}", sum);
+    Ok(())
 }
